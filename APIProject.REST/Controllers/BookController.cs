be76339d7c342dd38fc76e4531f36d52083b8e74
Samuel Ivan <1081504@ucn.dk>
2022-04-12 11:17:04 +0200

    Started REST API demo.

---
+++ APIProject.REST/Controllers/BookController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject.REST.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+}
--- APIProject.REST/Program.cs
+++ APIProject.REST/Program.cs
@@ -1,11 +1,30 @@
+using APIProject.Repository;
+using APIProject.Repository.Interfaces;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<ILibraryRepository, LibraryRepository>();
+builder.Services.AddSingleton<IBookRepository, BookRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "REST API Demo",
+            Description = "REST API Level 3 Demo Application in .NET 6",
+        });
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
 
 var app = builder.Build();
 
