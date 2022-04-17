2788d46800323be1e2d6744ae51b10ee884766eb
Samuel Ivan <1081504@ucn.dk>
2022-04-17 17:22:12 +0200

    Started gRPC service.

---
+++ APIProject.Consumer/APIConsumer.cs
@@ -0,0 +1,11 @@
+﻿namespace APIProject.Consumer;
+
+public static class APIConsumer
+{
+    public static async Task<string> ConsumeREST(string url)
+    {
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync(url);
+        return await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ APIProject.Consumer/Program.cs
@@ -0,0 +1,3 @@
+﻿using APIProject.Consumer;
+string responseREST = await APIConsumer.ConsumeREST(url: "https://localhost:7206/api/book");
+Console.WriteLine(responseREST);
