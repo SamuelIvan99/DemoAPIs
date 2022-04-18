7a501e8d52426b33a9eebd503342ed0b5f766208
Samuel Ivan <1081504@ucn.dk>
2022-04-18 20:00:42 +0200

    Started performance testing.

---
+++ APIProject.Consumer/PerformanceTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace APIProject.Consumer
+{
+    public static class PerformanceTest
+    {
+        public static async Task testRESTCreate()
+        {
+            int n = 10;
+            int count = 10;
+            for (int j = 0; j < n; j++)
+            {
+                Stopwatch s = new Stopwatch();
+                s.Start();
+                for (int i = 0; i < count; i++)
+                {
+                    var restBook = new APIProject.Consumer.Models.Book { IBAN = $"{i + (i * j)}", Title = "Book1", Author = "Author1", PagesNo = 100 };
+                    await APIConsumer.RESTCreate(restBook);
+                }
+                s.Stop();
+                long time = s.ElapsedMilliseconds;
+                //double time = t.check() * 1e9 / count;
+                Console.WriteLine($"{j}: {time} ms");
+            }
+        }
+    }
+}
--- APIProject.Consumer/Program.cs
+++ APIProject.Consumer/Program.cs
@@ -3,30 +3,32 @@
 Console.WriteLine("REST API Demo");
 Console.WriteLine("------------------------------------------");
 
-var restBook = await APIConsumer.RESTGetById(id: "1");
-Console.WriteLine($"Book - IBAN: {restBook?.IBAN}, Title: {restBook?.Title}, Author: {restBook?.Author}, Pages: {restBook?.PagesNo}");
 
-restBook = new APIProject.Consumer.Models.Book { IBAN = "1", Title = "Book1", Author = "Author1", PagesNo = 100 };
-var restResult = await APIConsumer.RESTCreate(restBook);
-Console.WriteLine($"Created: {restResult}");
+//var restBook = await APIConsumer.RESTGetById(id: "1");
+//Console.WriteLine($"Book - IBAN: {restBook?.IBAN}, Title: {restBook?.Title}, Author: {restBook?.Author}, Pages: {restBook?.PagesNo}");
 
-var restBook1 = await APIConsumer.RESTGetById(id: "1");
-Console.WriteLine($"Book - IBAN: {restBook1?.IBAN}, Title: {restBook1?.Title}, Author: {restBook1?.Author}, Pages: {restBook1?.PagesNo}");
+//restBook = new APIProject.Consumer.Models.Book { IBAN = "1", Title = "Book1", Author = "Author1", PagesNo = 100 };
+//var restResult = await APIConsumer.RESTCreate(restBook);
+//Console.WriteLine($"Created: {restResult}");
+
+//var restBook1 = await APIConsumer.RESTGetById(id: "1");
+//Console.WriteLine($"Book - IBAN: {restBook1?.IBAN}, Title: {restBook1?.Title}, Author: {restBook1?.Author}, Pages: {restBook1?.PagesNo}");
+await PerformanceTest.testRESTCreate();
 
 Console.WriteLine("------------------------------------------");
 Console.WriteLine("\n");
 
 Console.WriteLine("gRPC API Demo");
 Console.WriteLine("------------------------------------------");
-var grpcBook = await APIConsumer.gRPCGetById(id: "1");
-Console.WriteLine($"Book - IBAN: {grpcBook?.Iban}, Title: {grpcBook?.Title}, Author: {grpcBook?.Author}, Pages: {grpcBook?.PagesNo}");
+//var grpcBook = await APIConsumer.gRPCGetById(id: "1");
+//Console.WriteLine($"Book - IBAN: {grpcBook?.Iban}, Title: {grpcBook?.Title}, Author: {grpcBook?.Author}, Pages: {grpcBook?.PagesNo}");
 
-grpcBook = new Book { Iban = "1", Title = "Book1", Author = "Author1", PagesNo = 100 };
-var grpcResult = await APIConsumer.gRPCCreate(grpcBook);
-Console.WriteLine(grpcResult);
+//grpcBook = new Book { Iban = "1", Title = "Book1", Author = "Author1", PagesNo = 100 };
+//var grpcResult = await APIConsumer.gRPCCreate(grpcBook);
+//Console.WriteLine(grpcResult);
 
-var grpcBook1 = await APIConsumer.gRPCGetById(id: "1");
-Console.WriteLine($"Book - IBAN: {grpcBook1?.Iban}, Title: {grpcBook1?.Title}, Author: {grpcBook1?.Author}, Pages: {grpcBook1?.PagesNo}");
+//var grpcBook1 = await APIConsumer.gRPCGetById(id: "1");
+//Console.WriteLine($"Book - IBAN: {grpcBook1?.Iban}, Title: {grpcBook1?.Title}, Author: {grpcBook1?.Author}, Pages: {grpcBook1?.PagesNo}");
 
 Console.WriteLine("------------------------------------------");
 Console.WriteLine("\n");
